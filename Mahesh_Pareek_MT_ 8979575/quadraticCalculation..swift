1e2207e92e9a817686eef9c7afb2f80f48aec2c4
Mahesh Pareek <pareekm644@gmail.com>
2024-03-11 19:30:37 -0400

    Mid Exam App Created

---
+++ Mahesh_Pareek_MT_ 8979575/quadraticCalculation..swift
@@ -0,0 +1,29 @@
+//
+//  quadraticCalculation..swift
+//  Mahesh_Pareek_MT_ 8979575
+//
+//  Created by Mahesh Pareek on 2024-03-11.
+//
+
+import UIKit
+
+class quadraticCalculation_: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
